5392ed8e22dc6cf085b5d525b0ae094b77a7e295
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-25 08:26:54 -0500

    Prevent iterating over empty pages

--- spider/spider.py
+++ spider/spider.py
@@ -10,11 +10,16 @@ class Spider(scrapy.Spider):
     start_urls = ['http://medicalxpress.com/sort/date/all/']
 
     def parse(self, response):
+        num_yielded = 0
         for article in response.css('.news-link'):
             url = article.css('a::attr("href")').get('')
             path = urlparse(url).path
             path = Path(path).stem
             yield {'Path': path, 'Title': article.css('a::text').get()}
+            num_yielded += 1
+        if num_yielded == 0:
+            # Pages with no articles were discovered toward the end, signaling the end.
+            return
 
         next_page = response.css('li.page-item:nth-child(2) a::attr("href")').get()
         if next_page is not None:
